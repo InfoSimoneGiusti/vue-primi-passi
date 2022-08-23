b7c7c4ba20a7c802b5f89a0ba7572d6dbf5bcf04
Simone Giusti <info@simonegiusti.it>
2022-08-23 13:00:13 +0200

    live del mattino

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Ciao ragazzi!</h1>
+    <HelloVue />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloVue from '@/components/HelloVue.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloVue
   }
 }
 </script>
---
+++ src/components/HelloVue.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h3>Benvenuta classe 69 in vue cli!</h3>
+        <h4>Questo è il mio primo progetto!</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloVue'
+    }
+</script>
+
+<style>
+
+</style>
